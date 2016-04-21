ace51e919e33fd28663857d3a2104ee994ae13a4
Steven Tilley <stilley2@jhmi.edu>
2016-04-21 00:10:17 -0400

    add home parameters

--- +APT/VerticalStage.m
+++ +APT/VerticalStage.m
@@ -11,11 +11,20 @@ classdef VerticalStage < APT.HomeableStage
         POSMIN = 0
         VELMAX = 3
         VELMIN = 0
+
+	% these values taken from what thorlabs user sends ot the stage
+	HOMEDIRECTION = APT.HomeableStage.CHAR(2)
+	HOMELIMITSWITCH = APT.HomeableStage.CHAR(1)
+	HOMEVELOCITY = APT.HomeableStage.LONG(65958132)
+	HOMEOFFSETDISTANCE = APT.HomeableStage.LONG(245760)
+
     end
 
     methods
         function obj = VerticalStage(port, varargin)
             obj@APT.HomeableStage(port, varargin{:})
+	    data = {obj.CHAR(obj.channel), obj.HOMEDIRECTION, obj.HOMELIMITSWITCH, obj.HOMEVELOCITY, obj.HOMEOFFSETDISTANCE};
+	    obj.send(obj.MOT_SET_HOMEPARAMS, 'data', data);
         end
     end
 
